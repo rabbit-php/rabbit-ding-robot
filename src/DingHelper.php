327457636c29332534cede83f8201e005d03e465
Albert <amuluowin@163.com>
2020-08-22 10:38:57 +0800

    add helper

---
+++ src/DingHelper.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace Ding;
+
+namespace Rabbit\Ding\Robot;
+
+use Rabbit\Base\Core\Exception;
+
+/**
+ * Class DingHelper
+ * @package Ding
+ */
+class DingHelper
+{
+    /**
+     * @param array $response
+     * @throws Exception
+     */
+    public static function checkResponse(array $response): void
+    {
+        if ($response['errcode'] !== 0) {
+            throw new Exception($response['errmsg']);
+        }
+    }
+}
